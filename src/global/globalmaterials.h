c406b0bd2fd90ca56fe5750f4764455771d5c1b4
Vadym Sidorov <lafleurbox@gmail.com>
2017-01-15 15:39:24 +0200

    Specified new material in globalmaterials.

--- src/global/globalmaterials.h
+++ src/global/globalmaterials.h
@@ -2,16 +2,21 @@
 #define TMC_MATERIALS_H
 
 #include <Qt3DExtras>
+#include "customalphaphong.h"
 
 namespace TooManyCubes {
 
 class GlobalMaterials {
 public:
     GlobalMaterials() = delete;
-    static Qt3DExtras::QPhongMaterial DEFAULT_MATERIAL; // TODO: figure out how to prevent changing without const qualifier
+
+    // TODO: figure out how to prevent changing without const qualifier
+    static Qt3DExtras::QPhongMaterial DEFAULT_MATERIAL;
+    static CustomAlphaPhong DEFAULT_TRANSP_MATERIAL;
 };
 
 Qt3DExtras::QPhongMaterial GlobalMaterials::DEFAULT_MATERIAL;
+CustomAlphaPhong GlobalMaterials::DEFAULT_TRANSP_MATERIAL;
 
 }
 
